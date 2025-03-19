69a705b1e8296c3d23c803de7a95d8e83df4e0af
Vedat Akcan <akcanvedat47@gmail.com>
2025-03-19 03:20:27 +0300

    Added ViewBinding, Navigation, ViewModel, LiveData, and Material Design dependencies.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,7 +33,13 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    buildFeatures {
+        viewBinding = true
+    }
+
 }
+
+
 
 dependencies {
 
@@ -45,4 +51,17 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+
+    // Navigation Component
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+
+    // ViewModel and LiveData
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+
+    // Material Design
+    implementation(libs.material.v150)
+
 }
