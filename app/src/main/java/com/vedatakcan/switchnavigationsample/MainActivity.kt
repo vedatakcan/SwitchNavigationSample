5383ed4d42abeb20e8fea67f76fd1c3657a0d7b1
Vedat Akcan <akcanvedat47@gmail.com>
2025-03-20 06:24:55 +0300

    Integrate bottom menu navigation

--- app/src/main/java/com/vedatakcan/switchnavigationsample/MainActivity.kt
+++ app/src/main/java/com/vedatakcan/switchnavigationsample/MainActivity.kt
@@ -1,20 +1,72 @@
 package com.vedatakcan.switchnavigationsample
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.vedatakcan.switchnavigationsample.databinding.ActivityMainBinding
+import com.vedatakcan.switchnavigationsample.viewmodel.AppViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+
+        // NavHostFragment'ı al
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> {
+                    // Eğer zaten HomeFragment'te isek, navigasyon yapma
+                    if (navController.currentDestination?.id != R.id.homeFragment) {
+                        navController.navigate(R.id.homeFragment)
+                    }
+                    true
+                }
+
+                R.id.settingsFragment -> {
+                    navController.navigate(R.id.settingsFragment)
+                    true
+                }
+
+                R.id.helpFragment -> {
+                    navController.navigate(R.id.helpFragment)
+                    true
+                }
+
+                R.id.notificationsFragment -> {
+                    navController.navigate(R.id.notificationsFragment)
+                    true
+                }
+
+                R.id.profileFragment -> {
+                    navController.navigate(R.id.profileFragment)
+                    true
+                }
+
+                R.id.searchFragment -> {
+                    navController.navigate(R.id.searchFragment)
+                    true
+                }
+
+                else -> false
+
+            }
         }
     }
 }
+
